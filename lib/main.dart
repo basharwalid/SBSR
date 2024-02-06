e9c2ccbbae7d77bc47a57c01582341d4766ac666
Donniia <da6990669@gmail.com>
2024-02-07 01:29:17 +0200

    login and signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sbsr/Core/Providers/ThemeProvider.dart';
@@ -6,12 +7,18 @@ import 'package:sbsr/Presentation/UI/GetStarted/IntroView.dart';
 import 'package:sbsr/Presentation/UI/HomeScreen/HomeView.dart';
 import 'package:sbsr/Presentation/UI/Splash/Splash.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'Presentation/UI/Registration/ForgetPassword/forgetpassword_view.dart';
+import 'Presentation/UI/Registration/LogIn/login_view.dart';
+import 'Presentation/UI/Registration/SignUp/signup_view.dart';
+import 'firebase_options.dart';
 
-void main() async{
-  runApp(
-  ChangeNotifierProvider(create: (BuildContext context) => ThemeProvider(),
-  child: MyApp())
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(ChangeNotifierProvider(
+      create: (BuildContext context) => ThemeProvider(), child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -23,23 +30,26 @@ class MyApp extends StatelessWidget {
     themeProvider = Provider.of<ThemeProvider>(context);
     setTheme();
     return MaterialApp(
-        initialRoute: Splash.routeName,
-        debugShowCheckedModeBanner: false,
-        routes: {
-          Splash.routeName : (_)=> Splash(),
-          IntroView.routeName : (_) => IntroView(),
-          HomeView.routeName : (_) => HomeView(),
-        },
+      initialRoute: Splash.routeName,
+      debugShowCheckedModeBanner: false,
+      routes: {
+        Splash.routeName: (_) => Splash(),
+        IntroView.routeName: (_) => IntroView(),
+        HomeView.routeName: (_) => HomeView(),
+        LoginScreen.routeName: (_) => LoginScreen(),
+        SignUpScreen.routeName: (_) => SignUpScreen(),
+        ForgetPasswordScreen.routeName: (_) => ForgetPasswordScreen()
+      },
       theme: MyTheme.greenTheme,
       darkTheme: MyTheme.purpleTheme,
       themeMode: themeProvider.getTheme(),
     );
   }
 
-  Future<void>setTheme() async{
+  Future<void> setTheme() async {
     SharedPreferences preferences = await SharedPreferences.getInstance();
     var theme = preferences.getString("theme");
-    themeProvider.changeTheme(theme == "Dark" || theme == null? ThemeMode.dark : ThemeMode.light);
+    themeProvider.changeTheme(
+        theme == "Dark" || theme == null ? ThemeMode.dark : ThemeMode.light);
   }
 }
-
